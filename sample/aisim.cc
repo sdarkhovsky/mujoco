06b2c992d7785ae5418a3166f2585e6257f83290
Samuel Darkhovsky <darkhovsky@usa.net>
2023-12-15 23:14:28 -0500

    added communication thread

--- sample/aisim.cc
+++ sample/aisim.cc
@@ -16,6 +16,15 @@
 #include <cstring>
 #include <vector>
 #include <chrono>
+#include <thread>
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
 
 #include <GLFW/glfw3.h>
 #include <mujoco/mujoco.h>
@@ -133,6 +142,70 @@ void send_controls(const char* actuator_name, mjtNum actuator_val) {
     }
 }
 
+struct _communication_thread_params_ {
+  int terminate;
+  char buf[1000];
+};
+
+void communication_thread_func(_communication_thread_params_* communication_thread_params)
+{
+  int server_fd, new_socket;
+  ssize_t valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  socklen_t addrlen = sizeof(address);
+  char buffer[1024] = { 0 };
+
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket failed");
+    return;
+  }
+
+  // Forcefully attaching socket to the port 8080
+  if (setsockopt(server_fd, SOL_SOCKET,
+        SO_REUSEADDR | SO_REUSEPORT, &opt,
+        sizeof(opt))) {
+    perror("setsockopt");
+    return;
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+
+  // Forcefully attaching socket to the port 8080
+  if (bind(server_fd, (struct sockaddr*)&address,
+      sizeof(address))
+    < 0) {
+    perror("bind failed");
+    return;
+  }
+
+  while(!communication_thread_params->terminate) {
+    if (listen(server_fd, 3) < 0) {
+      perror("listen");
+      return;
+    }
+    if ((new_socket
+      = accept(server_fd, (struct sockaddr*)&address,
+          &addrlen))
+      < 0) {
+      perror("accept");
+      return;
+    }
+    valread = read(new_socket, buffer,
+          1024 - 1); // subtract 1 for the null
+                // terminator at the end
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+  }
+
+  // closing the connected socket
+  close(new_socket);
+  // closing the listening socket
+  close(server_fd);
+  return 0;
+}
 
 /*
 command line examples:  
@@ -193,6 +266,10 @@ int main(int argc, const char** argv) {
   glfwSetMouseButtonCallback(window, mouse_button);
   glfwSetScrollCallback(window, scroll);
 
+  _communication_thread_params_ communication_thread_params;
+  std::memset(&communication_thread_params, 0, sizeof(communication_thread_params));
+  std::thread communication_thread = std::thread(communication_thread_func, &communication_thread_params);  
+
   using namespace std::chrono;
   auto prev_time = system_clock::now();
 
@@ -220,6 +297,10 @@ int main(int argc, const char** argv) {
     // process pending GUI events, call GLFW callbacks
     glfwPollEvents();
   }
+
+  communication_thread_params.terminate = 1;
+  communication_thread.join();  
+
 
   //free visualization storage
   mjv_freeScene(&scn);
